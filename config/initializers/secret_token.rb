e337a602d9709d63d4beaec4b049ae95b4397bcc
Tommy O'Neil <alieander@gmail.com>
2013-03-12 14:26:34 -0700

    app configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardgame::Application.config.secret_token = 'd0aaff7e721b9d439182e4e52a0240f821870209f2b87abffcef8c82ced140928ad61e022228beef0fcd1c48e36a5830ee23c538a5922c42048a02f530447c14'
